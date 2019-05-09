0d66792209ba44ae3474f591a2bc9c1811315e3a
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-10 01:46:29 +0300

    One Scene Added.

---
+++ Assets/Scripts/Scene9Scripts/RulerDrag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulerDrag : MonoBehaviour
+{
+    private bool drag;
+    private Vector3 firstPosition;
+
+    void Start()
+    {
+        drag = false;
+        firstPosition = this.transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        drag = true;
+    }
+
+    void OnMouseDrag()
+    {
+        if (drag)
+        {
+            Vector3 currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.position = new Vector3(currentPos.x, currentPos.y, 0);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        drag = false;
+        this.transform.position = firstPosition;
+    }
+}
