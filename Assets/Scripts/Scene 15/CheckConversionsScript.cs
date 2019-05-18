384900c9fb09afe307244e063872fe627f706263
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-18 15:10:34 +0300

    Conversion Activity Added.

---
+++ Assets/Scripts/Scene 15/CheckConversionsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckConversionsScript : MonoBehaviour
+{
+    public InputField First, Second, Third, Fourth;
+    public GameObject Success, Failure;
+
+    public void CheckValues()
+    {
+        if (First.text == "4" && Second.text == "50"
+            && Third.text == "300" && Fourth.text == "8")
+        {
+            Success.SetActive(true);
+        }
+        else
+        {
+            Failure.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene 15/ConverterScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConverterScript : MonoBehaviour
+{
+    public InputField FirstPartInput, SecondPartInput;
+    public Text FirstPartResult, SecondPartResult;
+    public GameObject ConverterPanel, WarningPanel, OrderWarningPanel, MachineBreakdownPanel;
+    public GameObject FirstConvertButton, SecondConvertButton, ThirdConvertButton, FourthConvertButton;
+    private bool firstConverted, secondConverted;
+
+    public void ConvertFirst()
+    {
+        if (FirstPartInput.text == "40")
+        {
+            FirstPartResult.text = "4 cm";
+            firstConverted = true;
+        }
+        else
+        {
+            ConverterPanel.SetActive(false);
+            WarningPanel.SetActive(true);
+        }
+    }
+
+    public void ConvertSecond()
+    {
+        if (!firstConverted)
+        {
+            ConverterPanel.SetActive(false);
+            OrderWarningPanel.SetActive(true);
+        }
+        else if (SecondPartInput.text != "5")
+        {
+            ConverterPanel.SetActive(false);
+            WarningPanel.SetActive(true);
+        }
+        else
+        {
+            secondConverted = true;
+            SecondPartResult.text = "50 cm";
+        }
+    }
+
+    public void ConvertThird()
+    {
+        if (!secondConverted)
+        {
+            ConverterPanel.SetActive(false);
+            OrderWarningPanel.SetActive(true);
+        }
+        else
+        {
+            ConverterPanel.SetActive(false);
+            MachineBreakdownPanel.SetActive(true);
+            FirstConvertButton.SetActive(false);
+            SecondConvertButton.SetActive(false);
+            ThirdConvertButton.SetActive(false);
+            FourthConvertButton.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstConverted = secondConverted = false;
+        FirstPartResult.text = SecondPartResult.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
